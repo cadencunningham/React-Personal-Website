a3d2021a34c389ea4eb30555a6c4b5736984dc2a
Caden Cunningham <cac2752@louisiana.edu>
2020-12-03 15:47:55 -0600

    setup 7-1 scss styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Layout from './Layout';
-import './app.scss';
+import './scss/main.scss';
 
 const App = () => {
     return (
--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -8,7 +8,7 @@ const Layout = (props: IProps) => {
     const { children } = props;
     return (
         <div>
-            <div>HEADER</div>
+            <div className="header">HEADER</div>
             {children}
             <div>FOOTER</div>
         </div>
