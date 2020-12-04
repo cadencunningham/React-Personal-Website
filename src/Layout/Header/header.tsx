a2372148fee02672e3d0f8bdfbf6698d1db8d84d
Caden Cunningham <cac2752@louisiana.edu>
2020-12-04 12:32:42 -0600

    started header nav bar

--- src/Layout/Header/header.tsx
+++ src/Layout/Header/header.tsx
@@ -4,7 +4,13 @@ const Header = () => {
     return (
         <div className="header">
             <nav className="header-nav">
+                <button className="header-nav__button">Resume</button>
+                <button className="header-nav__button">Blog</button>
+
                 <label className="header-nav__name">Caden Cunningham</label>
+
+                <button className="header-nav__button">Socials</button>
+                <button className="header-nav__button">Contact Me</button>
             </nav>
 
             <div className="header__background-img" />
