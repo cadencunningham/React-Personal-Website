94685fb0b3dc1987d40b38ee00b7e6597aa88a52
Caden Cunningham <xcadencunningham@gmail.com>
2020-12-13 16:31:53 -0600

    bug fix for nav collapse

--- src/App/Layout/Navigation/index.tsx
+++ src/App/Layout/Navigation/index.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import Button from '../../shared/Button';
 
 let lastCurrentHeight = 0;
-const NAV_COLLAPSE_CLASS = 'nav__collaspe';
+const NAV_COLLAPSE_CLASS = 'nav__collapse';
+const NAV_NAME_CLASS = 'nav-name';
 
 const Navigation = () => {
     const [navExpandClass, setNavExpandClass] = useState('');
@@ -10,7 +11,6 @@ const Navigation = () => {
     useEffect(() => {
         document.addEventListener('scroll', () => {
             //Start Nav Name
-
             const windowHeight = window.innerHeight / 2;
             const currentHeight = window.pageYOffset;
             const heightDifference = windowHeight - currentHeight;
@@ -21,8 +21,7 @@ const Navigation = () => {
             style =
                 style + (newNameHeight >= 4.315 ? `top:${newNameHeight}vh` : `transform:translate(-50%,0%);top:unset;`);
 
-            document.getElementsByClassName('nav-name')[0].setAttribute('style', style);
-
+            document.getElementsByClassName(NAV_NAME_CLASS)[0].setAttribute('style', style);
             //End Nav Name
 
             if (heightDifference <= 0) {
@@ -40,14 +39,14 @@ const Navigation = () => {
     }, []);
 
     return (
-        <nav className={['nav', navExpandClass].join('')}>
+        <nav className={['nav', navExpandClass].join(' ')}>
             <div className="gen__width--100 gen__justify-content--sb gen__display--flex gen_align-items--center">
                 <div className="nav__button-container">
                     <Button text={'Home'} selected />
                     <Button text={'Jira Board'} />
                 </div>
 
-                <label className="nav-name">Caden Cunningham</label>
+                <label className={NAV_NAME_CLASS}>Caden Cunningham</label>
 
                 <div className="nav__button-container">
                     <Button text={'Resume'} />
