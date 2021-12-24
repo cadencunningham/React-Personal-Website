03b24a13d1cd9e4e378f90d38091259363af10e4
Caden Cunningham <54606165+cadencunningham@users.noreply.github.com>
2021-12-23 18:00:19 -0600

    fix for header circles

--- src/App/Layout/Header/index.tsx
+++ src/App/Layout/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className="header">
+        <div className="header" style={{height:window.innerHeight}}>
             <div className="header__background-container">
                 <div className="header__background-layer--inner" />
                 <div className="header__background-layer--center-pulse" />
